0e6e239f184fecd487d188a8f7fc84bc747c6d7b
alansun17904 <asun17904@gmail.com>
2025-03-23 23:34:02 +0100

    updated gitignore

--- scripts/get_reprs.sh
+++ scripts/get_reprs.sh
@@ -2,6 +2,6 @@
 
 for i in {1..30}; do
     for seed in {1..10}; do
-        python3 src/reprs.py --case_id $i --model_id $seed --out_name src/data/reprs/case_${i}_model_${seed}.pkl
+        python3 src/reprs.py --case_id $i --model_id $seed --out_name src/data/reprs/var_case_${i}_model_${seed}.pkl
     done
 done
--- src/reprs.py
+++ src/reprs.py
@@ -7,23 +7,23 @@ from transformer_lens import HookedTransformer
 from circuits_benchmark.utils.get_cases import get_cases
 
 
-# load model 
-ROOT = Path("")
+# load model
+ROOT = Path("../circuits-benchmark/results/var_ll_models")
 
-CFG_PATH = ROOT / "ll_model_cfg_510.pkl"
+CFG_PATH = "ll_model_cfg_510.pkl"
 
 def main(case_id, model_id, out_name, batch_size=256):
 
     cases = get_cases()
     case = [c for c in cases if c.__class__.__name__[4:] in str(case_id)][0]
 
-    tf = HookedTransformer(pickle.load(open(CFG_PATH, "rb")))
-    case_model_dir = ROOT / f"{case_id}-{model_id}"
-
+    case_model_dir = ROOT / f"{case_id}-{model_id}/ll_models/{case_id}"
     if not os.path.exists(case_model_dir):
         sys.exit(1)
 
-    model_path = ROOT / f"{case_id}-{model_id}" / "ll_model_510.pth"
+    tf = HookedTransformer(pickle.load(open(case_model_dir / CFG_PATH, "rb")))
+
+    model_path = case_model_dir / "ll_model_510.pth"
 
     tf.load_state_dict(torch.load(model_path))
 
@@ -31,17 +31,16 @@ def main(case_id, model_id, out_name, batch_size=256):
     clean_data = case.get_clean_data(min_samples=200, max_samples=200)
     print("Probing", len(clean_data), "examples.")
     loader = clean_data.make_loader(batch_size=batch_size)
-    resids = None
-    logits = None
+    resids = []
     for x, _ in loader:
         _, cache = tf.run_with_cache(x)
         resid = cache.accumulated_resid()
-        if resid is None:
-            resid = resid
+        if resids is None:
+            resids = resid
         else:
-            resid = torch.cat([resids, resid], dim=1)
+            resids = torch.cat([resids, resid], dim=1)
         del cache
-    
+
     flats = resids.permute(1, 0, 2, 3).flatten(start_dim=1)
 
     pickle.dump(flats, open(out_name, "wb"))
