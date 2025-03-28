b20561bb40824b5128835edb3fc5287b711233bf
alansun17904 <asun17904@gmail.com>
2025-03-28 05:52:35 +0100

    4-setting scripts

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./src/main.py train iit --epochs 30 -s 0.4 -iit 1 -b 1 --output-dir src/data/var-ll-models
+./src/main.py train iit --epochs 30 -s 0.4 -iit 1 -b 1 --output-dir src/data/constant-ll-models
 ./src/main.py train iit --epochs 30 -s 0.4 -iit 1 -b 1 --rand-correspondence --output-dir src/data/corr-ll-models
 ./src/main.py train iit --epochs 30 -s 0.4 -iit 1 -b 1 --rand-architecture --output-dir src/data/arch-ll-models
 ./src/main.py train iit --epochs 30 -s 0.4 -iit 1 -b 1 --rand-correspondence --rand-architecture --output-dir src/data/corr-arch-ll-models
