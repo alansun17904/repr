a316012733c0a6ed65e5ab9611f42f4b9b813eb8
Alan Sun <asun17904@gmail.com>
2025-03-27 21:08:25 -0400

    dynamic training based on admissible cases

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
+import pickle
 import logging
 import sys
+from pathlib import Path
 
 import jax
 
@@ -16,11 +18,22 @@ logging.basicConfig(level=logging.ERROR)
 
 if __name__ == "__main__":
   parser = build_main_parser()
+
   args, _ = parser.parse_known_args(sys.argv[1:])
 
-  if args.command == "run":
-    run_algorithm.run(args)
-  elif args.command == "train":
-    train.run(args)
-  elif args.command == "eval":
-    evaluation.run(args)
+
+  # get the admissible cases
+  admissible = pickle.load(open("admissible_tasks.pkl", "rb"))
+
+  for case in admissible:
+    print(f"Training against Case {case.get_name()}")
+    for seed in range(0, 10):
+      args.seed = seed
+      args.indices= [case.get_name()]
+      args.output_dir = str(Path(args.output_dir) / f"c{case.get_name()}-s{seed}")
+      if args.command == "run":
+        run_algorithm.run(args)
+      elif args.command == "train":
+        train.run(args)
+      elif args.command == "eval":
+        evaluation.run(args)
